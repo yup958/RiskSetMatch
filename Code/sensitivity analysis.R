78011bd57f04473a02bbfb85b4d1628e2d4226ac
hhhanying <48273181+hhhanying@users.noreply.github.com>
2019-12-03 01:39:43 -0600

    Update sensitivity analysis.R

--- Code/sensitivity analysis.R
+++ Code/sensitivity analysis.R
@@ -1,11 +1,15 @@
 library(sensitivitymw)
 # senmw does matching with multiple controls (same number of controls)
-GammaSeq = seq(1,1.7,0.01)
+GammaSeq = seq(1,1.7,0.1)
+GammaSeq = seq(1.5,1.6,0.01)
 upperBound = matrix(0,length(GammaSeq),1)
-tem = cbind(trtgroup$deathflag, ctrlgroup$deathflag)
+tem = cbind(trt$Deathflag, con$Deathflag)
 for(i in 1:length(GammaSeq)) {
   upperBound[i,1] =senmw(tem, gamma = GammaSeq[i], method = "t")$pval
 }
 out = cbind(GammaSeq,upperBound)
 colnames(out) = c("Gamma","t-test")
 round(out,3)
+
+senmwCI(tem, gamma = 1.35, method = 't', one.sided = TRUE)
+
